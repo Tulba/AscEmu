45c9f3f36a6fc30962a4dddade065a8591569355
sanctum32 <andriuspel@gmail.com>
2017-10-25 04:33:15 +0300

    Update guards faction

--- sql/violet_hold.sql
+++ sql/violet_hold.sql
@@ -3,3 +3,6 @@ UPDATE gameobject_spawns SET orientation3 = 0.928246, orientation4 = 0.371966 WH
 
 -- Removed not needed npcs (they're spawned by script)
 delete from creature_spawns where entry in (31011, 31010, 31009, 31008, 31007, 30659) and map = 608;
+
+-- Update guards faction
+update creature_properties set faction = 1718 where entry = 30659;
