f0e5fa6898357e6a74224d8c9338fc87b704901f
sanctum32 <andriuspel@gmail.com>
2017-10-25 04:30:54 +0300

    Removed some npcs from vh
    They're handled by instance script generally

--- sql/violet_hold.sql
+++ sql/violet_hold.sql
@@ -1,2 +1,5 @@
 -- Update ichoron's gates
-UPDATE gameobject_spawns SET orientation3=0.928246, orientation4=0.371966 WHERE entry=191722 AND map=608;
+UPDATE gameobject_spawns SET orientation3 = 0.928246, orientation4 = 0.371966 WHERE entry = 191722 AND map = 608;
+
+-- Removed not needed npcs (they're spawned by script)
+delete from creature_spawns where entry in (31011, 31010, 31009, 31008, 31007, 30659) and map = 608;
